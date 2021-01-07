6acf1e375d8373348f1ed0aad603e74afbcd0e35
Aadi Kuchlous <aadikuchlous@gmail.com>
2021-01-07 17:18:39 +0530

    added excel functionality, with intro page

--- main.py
+++ main.py
@@ -42,14 +42,15 @@ def create_images(text):
 		prefix = ' '.join(words[0:i])
 		highlighted_word = '<span style="color:red;">' + words[i] + '</span>'
 		suffix = ' '.join(words[i + 1: len(words)])
-		img = '<div style="height: 720; display: -webkit-box; -webkit-box-pack: center;"><img src="img.jpg" style="width: 200px;"></img>'
+		img = '<div style="height: 360px; display: flex; justify-content: center; align-items: flex-end; padding: 10px"><img src="img.jpg" style="width: 200px;"></img></div>'
 		br = '<br>'
-		html = img + br + prefix + ' ' + highlighted_word + ' ' + suffix + '</div>'
+		text = '<div style="height: 360px; display: flex; justify-content: center; align-items: flex-start;"><p>{}</p></div>'.format(prefix + ' ' + highlighted_word + ' ' + suffix)
+		html = '<!DOCTYPE html><html><body><div id="vid_area" style="height: 720px; width: 1280px;"><h1 style="font-size: 3.5rem">{0}{1}{2}</h1></div></body></html>'.format(img, br, text)
 		with open("tmp.html", "w") as f:
 			f.write(html)
-		cmd = "node --trace-warnings pup.js file://{0}/tmp.html images/{1}.jpg".format(os.getcwd(), str(i))
-		images.append("images/{}".format(i))
+		cmd = "node pup.js file://{0}/tmp.html images/{1}.jpg".format(os.getcwd(), str(i))
 		os.system(cmd)
+		images.append("images/{}".format(i))
 
 def concatenate_videos(videos, output_name):
 	with open('con.in', 'w') as f:
@@ -77,26 +78,28 @@ def create_para_vid(speed, i, time_data, audio, output_name):
 
 		f.write("file \'images/{0}.jpg\' \n".format(len(end_times)))
 		f.write("duration {} \n".format(prev_time+0.5))
-	os.system("ffmpeg -y -i {0} -f concat -i ffmp.in -strict -2 -vsync vfr -pix_fmt yuv420p -vf fps=24 -video_track_timescale 90000 -max_muxing_queue_size 2048 -tune animation -crf 6 {1}".format(audio, "mid.mp4"))
-	os.system('ffmpeg -y -i mid.mp4 -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=white" {0}nf.mp4'.format(output_name))
+	os.system("ffmpeg -y -i {0} -f concat -i ffmp.in -strict -2 -vsync vfr -pix_fmt yuv420p -vf fps=24 -video_track_timescale 90000 -max_muxing_queue_size 2048 -tune animation -crf 6 {1}nf.mp4".format(audio, output_name))
+	# os.system('ffmpeg -y -i mid.mp4 -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=white" {0}nf.mp4'.format(output_name))
 	with open('blank.in', 'w') as f:
 		f.write('\n'.join(["file \'images/{0}.jpg\'".format(time_data_l-1), "duration {}".format("1")]))
-	os.system("ffmpeg -y -i {0} -f concat -i blank.in -strict -2 -vsync vfr -pix_fmt yuv420p -vf fps=24 -video_track_timescale 90000 -max_muxing_queue_size 2048 -tune animation -crf 6 {1}".format("blank.mp3", "mid.mp4"))
-	os.system('ffmpeg -y -i mid.mp4 -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=white" fill{}.mp4'.format(str(i+1)))
+	os.system("ffmpeg -y -i {0} -f concat -i blank.in -strict -2 -vsync vfr -pix_fmt yuv420p -vf fps=24 -video_track_timescale 90000 -max_muxing_queue_size 2048 -tune animation -crf 6 fill{1}.mp4".format("blank.mp3", str(i)))
+	# os.system('ffmpeg -y -i mid.mp4 -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=white" fill{}.mp4'.format(str(i+1)))
 	output_name_mp4 = output_name + '.mp4'
-	concatenate_videos(['{0}nf.mp4'.format(output_name), 'fill{}.mp4'.format(str(i+1))], output_name_mp4)
+	concatenate_videos(['{0}nf.mp4'.format(output_name), 'fill{}.mp4'.format(str(i))], output_name_mp4)
 
-def create_vids_from_text(file_dir):
-	text = open(file_dir, "r").read()
-	paras = text.split('\n')
+def create_vids_from_excel(file_dir):
+	os.system('mkdir images')
+	create_intro_video(file_dir)
 	normal_videos = []
 	slow_videos = []
 	split_videos = []
-	for i in range(len(paras)):
-		para = paras[i]
-		json_data = polly_json(para)
-		json_data_slow = polly_json(para)
-		audio_data = polly_audio(para)
+	sheet = read_excel(file_dir)
+	for i in range(3, sheet.max_row+1):
+		para = sheet.cell(row=i, column=1).value
+		polly_para = para.replace('*', '.')
+		json_data = polly_json(polly_para)
+		json_data_slow = polly_json(polly_para)
+		audio_data = polly_audio(polly_para)
 		with open('audio_uf.mp3', 'wb') as f:
 			f.write(audio_data.read())
 		os.system("ffmpeg -y -i {} -ar 48000 {}".format("audio_uf.mp3", "audio.mp3"))
@@ -107,15 +110,15 @@ def create_vids_from_text(file_dir):
 		with open('audio_uf.mp3', 'wb') as f:
 			f.write(audio_data_split.read())
 		os.system("ffmpeg -y -i {} -ar 48000 {}".format("audio_uf.mp3", "audio_split.mp3"))
-		images = create_images(para)
+		images = create_images(para.replace('*', ''))
 
-		create_para_vid(1, i, json_data, 'audio.mp3', "vid{}".format(str(i+1)))
-		create_para_vid(0.75, i, json_data_slow, 'audio_slow.mp3', "vid{}-slow".format(str(i+1)))
-		create_para_vid(1, i, json_data_split, 'audio_split.mp3', "vid{}-split".format(str(i+1)))
+		create_para_vid(1, i-2, json_data, 'audio.mp3', "vid{}".format(str(i-2)))
+		create_para_vid(0.75, i-2, json_data_slow, 'audio_slow.mp3', "vid{}-slow".format(str(i-2)))
+		create_para_vid(1, i-2, json_data_split, 'audio_split.mp3', "vid{}-split".format(str(i-2)))
 
-		normal_videos.append("vid{}.mp4".format(str(i+1)))
-		slow_videos.append("vid{}-slow.mp4".format(str(i+1)))
-		split_videos.append("vid{}-split.mp4".format(str(i+1)))
+		normal_videos.append("vid{}.mp4".format(str(i-2)))
+		slow_videos.append("vid{}-slow.mp4".format(str(i-2)))
+		split_videos.append("vid{}-split.mp4".format(str(i-2)))
 
 	os.system("mkdir final_videos")
 
@@ -123,12 +126,31 @@ def create_vids_from_text(file_dir):
 	concatenate_videos(slow_videos, "final_videos/final_slow.mp4")
 	concatenate_videos(split_videos, "final_videos/final_split.mp4")
 
-	os.system("rm *.mp4")
-	os.system("rm a*.mp3")
+	# os.system("rm *.mp4")
+	# os.system("rm a*.mp3")
 
 def create_intro_video(file):
-	#wb = load_workbook(filename = file)
-	pass
+	sheet = read_excel(file)
+	text = (sheet.cell(row=2, column=1).value).split('\n')
+	audio_data = polly_audio('. '.join(text))
+	with open('audio_uf.mp3', 'wb') as f:
+			f.write(audio_data.read())
+	os.system("ffmpeg -y -i {} -ar 48000 {}".format("audio_uf.mp3", "intro_audio.mp3"))
+	with open('intro.in', 'w') as f:
+		f.write('\n'.join(["file \'images/{0}.jpg\'".format("intro"), "duration {}".format(5)]))
+	headers = ''
+	for line in text:
+		headers += '<h1 style="font-size:2.5rem">{}</h1>'.format(line)
+	html = '<!DOCTYPE html><html><body><div id="vid_area" style="height: 720px; width:1280px; display: flex; flex-direction: column; justify-content: center; align-items: center;">{}</div></body></html>'.format(headers)
+	with open("intro.html", "w") as f:
+			f.write(html)
+	os.system("node pup.js file://{0}/intro.html images/{1}.jpg".format(os.getcwd(), "intro"))
+	os.system("ffmpeg -y -i {0} -f concat -i intro.in -strict -2 -vsync vfr -pix_fmt yuv420p -vf fps=24 -video_track_timescale 90000 -max_muxing_queue_size 2048 -tune animation -crf 6 {1}.mp4".format("intro_audio.mp3", "intro"))
+	return("intro.mp4")
 
 
-create_vids_from_text("text_input.txt")
+def read_excel(path):
+	wb = openpyxl.load_workbook(path)
+	return(wb["Sheet1"])
+
+create_vids_from_excel("input.xlsx")
--- pup.js
+++ pup.js
@@ -3,10 +3,18 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  // await page.setViewport({
+  //   width: 1280,
+  //   height: 720,
+  //   deviceScaleFactor: 1,
+  // });
   const html_path = process.argv[2]
   const image_path = process.argv[3];
   await page.goto(html_path);
-  await page.screenshot({path: image_path});
+  const selector = '#vid_area';
+  await page.waitForSelector(selector);
+  const element = await page.$(selector);  
+  await element.screenshot({path: image_path});
 
   await browser.close();
 })();
